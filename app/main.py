19f900c0a221ca7bde2aed8694d911accf3c9be1
AaronBrennan1 <68754265+AaronBrennan1@users.noreply.github.com>
2025-03-30 17:17:43 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from .models import Prediction
 from .predictor import predict_mechanism
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow your GitHub Pages domain
+origins = [
+    "https://aaronbrennan1.github.io",
+    "http://localhost:3000",  # Optional, for local testing
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all (less secure)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
